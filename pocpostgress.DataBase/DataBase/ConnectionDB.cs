0c689b0cf5ef961a2eada5effa424c295bd5e3f5
Anna Beatriz Bastos <127196700+Anna-hub19@users.noreply.github.com>
2024-10-28 14:45:17 -0300

    Add files via upload

---
+++ pocpostgress.DataBase/DataBase/ConnectionDB.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+
+namespace PocPostgress.DataBase.DataBase
+{
+    public class ConnectionDB 
+    {  // pega a connection string
+        private readonly string _DbConnectionString;
+       
+        public ConnectionDB(IConfiguration configuration) {
+            _DbConnectionString = configuration.GetSection("ConnectionStrings").GetSection("BancoDeDados").Value;
+
+        }
+        public string GetString()
+        {
+            return _DbConnectionString;
+        }
+
+    }
+            
+}
+        
+
+     
+
+    
+
